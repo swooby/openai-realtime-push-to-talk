2cd46e62ea62c7fbe716c9be159b19bb5c104ecf
Paul Peavyhouse <pv@swooby.com>
2025-02-10 18:54:37 -0800

    Cherry pick of upstream c6e00860

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -111,6 +111,13 @@ export default function App() {
     if (dataChannel) {
       dataChannel.close();
     }
+
+    peerConnection.current.getSenders().forEach((sender) => {
+      if (sender.track) {
+        sender.track.stop();
+      }
+    });
+    
     if (peerConnection.current) {
       peerConnection.current.close();
     }
